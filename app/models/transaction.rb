79e0f7f1cfcb3d2ca4e34eb40be65bbc43962667
Joana Lopes <joana.lps@icloud.com>
2023-11-21 14:02:23 +0100

    add equal_to for start & end date the same date

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -2,6 +2,6 @@ class Transaction < ApplicationRecord
   belongs_to :user
   belongs_to :piece
 
-  validates :start_date, date: { after_or_equal: Proc.new { Date.today }, message: 'must be today or after' }, on: :create
-  validates :end_date, date: { after: :start_date }
+  validates :start_date, date: { after_or_equal_to: Proc.new { Date.today }, message: 'must be today or after' }, on: :create
+  validates :end_date, date: { after_or_equal_to: :start_date }
 end
